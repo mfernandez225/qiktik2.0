ea618aa1c1d79cc827c8f60efdbfb8debde960d2
Rooster <othmanmuhidean@gmail.com>
2020-04-18 13:36:28 -0700

    initial model structure has been implemented

---
+++ models/Favorite.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const FavoriteSchema = new Schema({
+  symbol: String,
+  name: String,
+});
+
+const Favorite = mongoose.model("Favorite", FavoriteSchema);
+
+module.exports = Favorite;
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength = 6
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  stocks: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Favorite"
+    }
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
