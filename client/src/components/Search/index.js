27c05a366c46d5ef7397dd1122567e833375253d
Rooster <othmanmuhidean@gmail.com>
2020-04-18 12:53:45 -0700

    search bar component added

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,11 +1,60 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+import stocks from "../stockData";
+// ticker 3166-1 alpha-2
+// ⚠️ No support for IE 11
+function stockToFlag(tickerSymbol) {
+  return typeof String.fromSymbolPoint !== "undefined"
+    ? tickerSymbol
+        .toUpperCase()
+        .replace(/./g, (char) =>
+          String.fromSymbolPoint(char.charSymbolAt(0) + 127397)
+        )
+    : tickerSymbol;
+}
 
-const Search = () => {
+const useStyles = makeStyles({
+  option: {
+    fontSize: 15,
+    "& > span": {
+      marginRight: 10,
+      fontSize: 18,
+    },
+  },
+});
+
+export default function StockSelect() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Search Bar</h1>
-    </div>
+    <Autocomplete
+      id="country-select-demo"
+      style={{ width: 300, position: "relative" }}
+      options={stocks}
+      classes={{
+        option: classes.option,
+      }}
+      autoHighlight
+      getOptionLabel={(option) => option.name}
+      renderOption={(option) => (
+        <React.Fragment>
+          <span>{stockToFlag(option.symbol)}</span>
+          {option.name} ({option.symbol})
+        </React.Fragment>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search Stock Name"
+          variant="outlined"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: "new-password", // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
   );
-};
-
-export default Search;
+}
