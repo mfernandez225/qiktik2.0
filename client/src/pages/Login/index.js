0788b486237423ee3111b67afd085060d6e8bdc2
Kayla San Filippo <Kaylasf@Kaylas-MacBook-Air.local>
2020-04-28 22:44:09 -0700

    not responsive yet

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Alert } from "reactstrap";
+import { Alert ,Jumbotron, Button, Col, Row} from "reactstrap";
 import API from "../../utils/API";
 import { useHistory, Link } from "react-router-dom";
+import "./style.css";
 
 const Login = () => {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState();
@@ -22,35 +24,58 @@ const Login = () => {
       setLoginError("Invalid email or password");
     }
   };
+  
 
   return (
-    <div className="">
-      <h1 className="mt-5">Login Page</h1>
+   
+    
+
+   //loginBody is for css background image
+    <div id="loginBody" className="">
+      
+
+
+      
+       <Jumbotron id="jumbo"  className="  w-75">
+       
+      <h1   className="mt-1 fontMe text-white colorMe"  >Login</h1>
       {loginError && <Alert color="danger">{loginError}</Alert>}
-      <form className="m-5" onSubmit={handleSubmit}>
+    
+    
+      <form className="m-5 " onSubmit={handleSubmit}>
+      
         <input
-          className="input-group p-3"
+          className="input-group p-3 m-1 fontMe"
           name="email"
           placeholder="Login (Email)"
           value={email}
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
         <input
-          className="input-group p-3"
+          className="input-group p-3 m-1 fontMe"
           name="password"
           placeholder="Password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
-        <button className="btn btn-lg btn-dark m-5" type="submit">
+     
+      
+        <div className="text-right mt-2">
+        <button className="btn btn-lg btn-dark  m-2 fontMe " type="submit">
           Log In
         </button>
         <Link to="./Signup">
-          <button className="btn btn-lg btn-secondary m-5">Sign Up</button>
+          <button id="sup" className="btn btn-lg btn-secondary m-2   fontMe">Sign Up</button>
         </Link>
+        </div>
+        
       </form>
+      
+      </Jumbotron>
+
     </div>
+
   );
 };
 
