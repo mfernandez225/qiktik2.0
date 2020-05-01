ffd644dbd9ecd0804ab1037f4ea597e3267ea0be
Kayla San Filippo <Kaylasf@Kaylas-MacBook-Air.local>
2020-04-30 18:00:53 -0700

    responsive

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Alert,Jumbotron } from "reactstrap";
+import { Alert ,Jumbotron, Navbar,  NavbarBrand, Media} from "reactstrap";
 import API from "../../utils/API";
 import { useHistory } from "react-router-dom";
+import logo from "../../assets/imgs/qiktik2.png";
 
 const Signup = () => {
   const [firstName, setFirstName] = useState("");
@@ -24,30 +25,49 @@ const Signup = () => {
       setSignupError("Invalid email or password");
     }
   };
+  const imgStyle = {
+    maxHeight: 120,
+    maxWidth: 120,
+   
+  }
 
   return (
+    <div>
+       <Navbar  fixed="top" className="p-0 transparent" >
+    <NavbarBrand  className="mr-auto">  
+    
+     <Media left href="#" >
+        <Media object src={logo} alt="qiktik" style={imgStyle} className="p-1 ml-1"/>
+      </Media>
+  
+      </NavbarBrand>
+  
+  </Navbar>
+
+    
     <div id="loginBody"> 
-     <Jumbotron id="jumbo"  className="p-2 w-75">
+    
+     <Jumbotron id="jumbo"  className="p-2 ">
        
       <h1 className="mt-5 colorMe fontMe">Sign Up</h1>
       {signupError && <Alert color="danger">{signupError}</Alert>}
       <form className="m-5" onSubmit={handleSubmit}>
         <input
-          className="input-group p-3 fontMe text-center m-1"
+          className="input-group p-3 fontMeSmall text-center m-1"
           name="firstName"
           placeholder="First name"
           value={firstName}
           onChange={(e) => setFirstName(e.currentTarget.value)}
         />
         <input
-          className="input-group p-3 fontMe text-center m-1"
+          className="input-group p-3 fontMeSmall text-center m-1"
           name="lastName"
           placeholder="Last name"
           value={lastName}
           onChange={(e) => setLastName(e.currentTarget.value)}
         />
         <input
-          className="input-group p-3 fontMe text-center m-1"
+          className="input-group p-3 fontMeSmall text-center m-1"
           name="email"
           placeholder="Email"
           type="email"
@@ -55,7 +75,7 @@ const Signup = () => {
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
         <input
-          className="input-group p-3 fontMe text-center m-1 "
+          className="input-group p-3 fontMeSmall text-center m-1 "
           name="password"
           placeholder="Password"
           type="password"
@@ -63,12 +83,13 @@ const Signup = () => {
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
         <div className="text-right ">
-        <button className="btn btn-lg btn-dark mt-3 fontMe" id="sup" type="submit">
+        <button className="btn btn-lg btn-dark mt-3 fontMeSmall" id="sup" type="submit">
           Sign Up
         </button>
         </div>
       </form>
       </Jumbotron>
+    </div>
     </div>
   );
 };
