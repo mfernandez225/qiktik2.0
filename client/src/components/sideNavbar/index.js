f7ee8a65578b9fc16cfce2ae2657317c4d692fcf
Kayla San Filippo <Kaylasf@Kaylas-MacBook-Air.local>
2020-05-07 21:00:28 -0700

    logo

--- client/src/components/sideNavbar/index.js
+++ client/src/components/sideNavbar/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Nav, NavLink, Media } from "reactstrap";
+import { Nav, NavLink, Media, Row } from "reactstrap";
 
 import Logout from "../LogoutButton/index";
 
 import API from "../../utils/API";
-import logo from "../../assets/imgs/teal.png";
+import logoo from "../../assets/imgs/Qteal.png";
 import "./style.css";
 
 const SideNavbar = ({
@@ -23,9 +23,11 @@ const SideNavbar = ({
     >
       {/* LOGO IMAGE - css: tabs: alignment, logo: sizing, fontMe: big font, fontMeSmall: small font*/}
       <div className="text-left text-white tabs">
-        <Media left className="mb-2">
-          <Media object src={logo} alt="qiktik" id="homeLogo" />
+        <Row className="center ml-2">
+      <Media center >
+              <Media object src={logoo} alt="qiktik" id="Logoo" />
         </Media>
+        </Row>
         <hr />
         <h5 className="text-light fontMe">Tracked Stocks</h5>
         {/* Displaying users selected favorite stocks */}
